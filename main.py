b70cde41ed096db2dcb2c9a2abdf363ac49420f4
Leandro <leandro.rawicz@coinfabrik.com>
2019-06-03 15:10:47 -0300

    transfer Ether

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ kovan = 2
 goerli = 3
 rinkeby = 4
 localhost = 5
-
-
-entorno = ropsten
-
-with open('Users.json') as json_file:  
-    data = json.load(json_file)
-
-
 entorno_URL = [ "https://mainnet.infura.io/v3/" , \
 			 	"https://ropsten.infura.io/v3/", \
 				"https://kovan.infura.io/v3/", \
@@ -25,23 +17,46 @@ entorno_URL = [ "https://mainnet.infura.io/v3/" , \
 				"http://localhost:8545"]
 
 
+entorno = ropsten
+
+with open('Users.json') as json_file:  
+    data = json.load(json_file)
+
+
+
+
 if (entorno == localhost):
 	URL = entorno_URL[entorno]
 else:
 	URL = entorno_URL[entorno] + data["Infura"]["projectID"]
 
+data["Ethereum"]["0"]["address"]
+w3 = Web3(Web3.HTTPProvider(URL))	
 
-web3 = Web3(Web3.HTTPProvider(URL))	
-
-print(web3.isConnected())
-
-
-print(web3.eth.getBlock(12345))
+print(w3.isConnected())
 
 
+print(w3.eth.getBlock(12345))
+
+####################################
+######## Transfer Ethers ###########
+####################################
+
+print("gas Value", w3.eth.gasPrice)
+
+signed_txn = w3.eth.account.signTransaction(dict(
+    nonce=w3.eth.getTransactionCount(data["Ethereum"]["0"]["address"]),
+    gasPrice = w3.eth.gasPrice, 
+    gas = 100000,
+    to=data["Ethereum"]["1"]["address"],
+    value=w3.toWei(0.02,'ether')
+  ),
+  data["Ethereum"]["0"]["priv"])
+
+w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
 
-
+print("transaction pending")
 
 #setter.main()
 #print("conectado", connected)
